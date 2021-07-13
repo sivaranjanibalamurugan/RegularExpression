1512d0b72c4c0346c33a6b2c9bbf5a598e3175e4
unknown <sivaranjanib99@gmail.com>
2021-07-13 21:56:12 +0530

    [siva]UC1 checking the validation for pincode

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -7,6 +7,12 @@ namespace RegularExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to regular expression!");
+            Pincode pincode = new Pincode();
+            string[] pinCodes = { "400088", "A400088", "400088B" };
+            for (int i = 0; i < pinCodes.Length; i++)
+            {
+                pincode.CheckPin(pinCodes[i]);
+            }
         }
     }
 }
