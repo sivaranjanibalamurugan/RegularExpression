b9835f22da959c5a4464f5a9a9d32a338eba5851
unknown <sivaranjanib99@gmail.com>
2021-07-14 12:39:25 +0530

    [siva]UC! validating the mail

---
+++ MailValidation/EmailValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MailValidation
+{
+    class EmailValidation
+    {
+        public void EmailCheck()
+        {
+            string pattern = @"^[a-zA-Z]{3}[a-zA-Z0-9]*";
+            string[] mail = { "xyz", "xyzabc8", "S" };
+            Regex regex = new Regex(pattern);
+            for (int i = 0; i < mail.Length; i++)
+            {
+                if (regex.IsMatch(mail[i]))
+                {
+                    Console.WriteLine("{0}-->Valid", mail[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}-->Invalid", mail[i]);
+                }
+            }
+
+        }
+    }
+}
